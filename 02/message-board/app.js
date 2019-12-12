120fb46db2672430a4cf882797ad716ae1ec80ef
miaoruifeng <mrfnet@163.com>
2019-12-12 22:24:06 +0800

    '走通页面之间跳转和404页面处理'

--- 02/message-board/app.js
+++ 02/message-board/app.js
@@ -17,12 +17,26 @@ http
         }
         res.end(data)
       })
+    } else if (url === '/post') {
+      fs.readFile('./views/post.html', (err, data) => {
+        if (err) {
+          return res.end('404 Not Found.')
+        }
+        res.end(data)
+      })
     } else if (url.indexOf('/public/') === 0) {
       // 例如： /public/css/main.css、 /public/js/main.js 等
       // 统一处理：如果请求路径是 /public 开头的，则认为要获取 public 中的某个资源，
       // 则直接可以把请求路径当作文件路径来直接进行读取
       console.log(url)
       fs.readFile('.' + url, (err, data) => {
+        if (err) {
+          return res.end('404 Not Found.')
+        }
+        res.end(data)
+      })
+    } else {
+      fs.readFile('./views/404.html', (err, data) => {
         if (err) {
           return res.end('404 Not Found.')
         }
