b453ca1a35a107376fc94ba68f05e5ac374279b1
miaoruifeng <mrfnet@163.com>
2019-12-18 11:52:16 +0800

    用POST请求数据实现留言板功能

--- 03/02-express-demo/app.js
+++ 03/02-express-demo/app.js
@@ -12,7 +12,19 @@ app.get('/about', (req, res) => {
 })
 
 // 公开指定目录
-app.use('/public/', express.static('./public/'))
+// 访问 /public/public资源  eg: 127.0.0.1/public/img/rb.jpg
+app.use('/public', express.static('./public'))  // 推荐使用
+// 文件相对路径可以省略 ./
+// app.use('/public', express.static('public'))
+
+
+// 访问 /public资源   eg: 127.0.0.1/img/rb.jpg
+// app.use(express.static('public'))
+
+
+// 给 public 起了别名 static
+// 访问 /static/public资源  eg: 127.0.0.1/static/img/rb.jpg
+// app.use('/static', express.static('public'))
 
 app.listen(port, () => {
   console.log(`App is running at ${port}`)
--- 03/message-board/app.js
+++ 03/message-board/app.js
@@ -1,9 +1,26 @@
 const express = require('express')
-const fs = require('fs')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+// 配置使用 art-template 模板引擎
+// 第一个参数表示，当渲染以 .html 结尾的文件时，使用 art-template 模板引擎
+// express-art-template 是 art-template 专门在 express 中使用的
 app.engine('html', require('express-art-template'))
+
+// express 为 response 提供了一个 render() 方法
+// render() 默认是不可以使用的，但是如果配置了 art-template 就可以使用了
+// render('html模板名', { 模板数据 })
+// 第一个参数不能写路径，默认回去项目中的 views 目录中查找该模板文件
+// 也就是 express 有一个约定：把所有的视图文件都放在 views 目录中
+
+// 如果想要修改默认的 views 目录，则可以：
+// app.set('views', render函数的路径)
+
+
+// 配置 body-parser 中间件
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const comments = [{
     name: '张三',
@@ -37,34 +54,48 @@ app.use('/public', express.static('./public'))
 // app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  // res.send('留言板案例')
-  fs.readFile('./views/index.html', (err, data) => {
-    res.render('index.html', {
-      comments
-    })
+  res.render('index.html', {
+    comments
   })
 })
 
 app.get('/post', (req, res) => {
-  fs.readFile('./views/post.html', (err, data) => {
-    // res.end(data)
-    res.render('post.html')
-  })
+  res.render('post.html')
 })
 
-app.get('/comment', (req, res) => {
-  // console.log(req.query)
-  const commentObj = req.query
-  commentObj.dateTime = '2019-12-17'
+// GET 方法
+// app.get('/comment', (req, res) => {
+//   // console.log(req.query)
+//   // req.query 只能拿 get 请求参数
+//   const commentObj = req.query
+//   commentObj.dateTime = '2019-12-17'
+//   comments.unshift(commentObj)
+//   res.redirect('/')
+// })
+
+// POST
+// 利用不同的请求方法，可以让一个请求路径使用多次
+app.post('/post', (req, res) => {
+  // 1. 获取表单 POST 请求体数据
+  // 2. 处理
+  // 3. 发送响应
+  // console.log(req.body)
+  const commentObj = req.body
+  commentObj.dateTime = '2019-12-18'
   comments.unshift(commentObj)
   res.redirect('/')
 })
 
+// admin 测试
+app.get('/admin', (req, res) => {
+  res.render('admin/index.html', {
+    title: '管理系统'
+  })
+})
+
 // 404
 app.use((req, res) => {
-  fs.readFile('./views/404.html', (err, data) => {
-    res.end(data)
-  })
+  res.render('404.html')
 })
 
 app.listen(port, () => {
