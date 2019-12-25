66286f118203bb739f51af6c50d7d5308e9bdce4
miaoruifeng <mrfnet@163.com>
2019-12-25 14:29:29 +0800

    mongoose实现MongoDB增删改查

--- 05/mongoose-demo/demo1.js
+++ 05/mongoose-demo/demo1.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+
+// 连接数据库
 mongoose.connect('mongodb://localhost:27017/test', {
   useNewUrlParser: true
 });
 
+// 创建一个模型 就是在设计数据库
+// MongoDB 是动态的，非常灵活，只需要在代码中设计数据库就可以了
 const Cat = mongoose.model('Cat', {
   name: String
 });
